27edd141a39a633b13e14d84c7733ceabd203353
AKuzminykh <hagen2006-75@yandex.ru>
2024-01-26 17:31:18 +0300

    agreements table commit

--- src/main/java/edu/example/demoDocker/models/Agreements.java
+++ src/main/java/edu/example/demoDocker/models/Agreements.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +15,24 @@ import lombok.NoArgsConstructor;
 public class Agreements {
     @Id @Column(name = "id") @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
     @Column(name = "agreement_id") private Long agreement_id;
+    @Column(name = "generalAgreementId") private String generalAgreementId;
+    @Column(name = "supplementaryAgreementId") private String supplementaryAgreementId;
+    @Column(name = "arrangementType") private String arrangementType;
+    @Column(name = "shedulerJobId") private Long shedulerJobId;
+    @Column(name = "number") private String number;
+    @Column(name = "openingDate") private Date openingDate;
+    @Column(name = "closingDate") private Date closingDate;
+    @Column(name = "cancelDate") private Date cancelDate;
+    @Column(name = "validityDuration") private Long validityDuration;
+    @Column(name = "cancellationReason") private String cancellationReason;
+    @Column(name = "status") private String status;
+    @Column(name = "interestCalculationDate") private Date interestCalculationDate;
+    @Column(name = "interestRate") private float interestRate;
+    @Column(name = "coefficient") private float coefficient;
+    @Column(name = "coefficientAction") private String coefficientAction;
+    @Column(name = "minimumInterestRate") private float minimumInterestRate;
+    @Column(name = "minimumInterestRateCoefficient") private String minimumInterestRateCoefficient;
+    @Column(name = "maximalnterestRate") private float maximalnterestRate;
+    @Column(name = "maximalnterestRateCoefficient") private float maximalnterestRateCoefficient;
+    @Column(name = "maximalnterestRateCoefficientAction") private String maximalnterestRateCoefficientAction;
 }
--- src/main/java/edu/example/demoDocker/service/dto/AgreementsDTO.java
+++ src/main/java/edu/example/demoDocker/service/dto/AgreementsDTO.java
@@ -1,8 +1,11 @@
 package edu.example.demoDocker.service.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +13,24 @@ import lombok.NoArgsConstructor;
 public class AgreementsDTO {
     private Long id;
     private Long agreement_id;
+    private String generalAgreementId;
+    private String supplementaryAgreementId;
+    private String arrangementType;
+    private Long shedulerJobId;
+    private String number;
+    private Date openingDate;
+    private Date closingDate;
+    private Date cancelDate;
+    private Long validityDuration;
+    private String cancellationReason;
+    private String status;
+    private Date interestCalculationDate;
+    private float interestRate;
+    private float coefficient;
+    private String coefficientAction;
+    private float minimumInterestRate;
+    private String minimumInterestRateCoefficient;
+    private float maximalnterestRate;
+    private float maximalnterestRateCoefficient;
+    private String maximalnterestRateCoefficientAction;
 }
